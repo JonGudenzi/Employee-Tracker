bab861539a74a351f6e205cf4a222768e65d19dc
JonGudenzi <jongudenzi@gmail.com>
2021-01-19 18:03:55 -0500

    added seed.sql file.  Started initial question when app is started

--- employeeDbConnection.js
+++ employeeDbConnection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql');
+const enquirer = require('enquirer');
+
 const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Gudenzi1955!',
+    password: '',
     database: 'employee_tracker_db',
-
 });
 
 connection.connect((err) => {
@@ -13,3 +14,22 @@ connection.connect((err) => {
     console.log('connected as id ' + connection.threadId);
     connection.end();
 });
+
+
+const startQuestion = [
+    {
+        type: 'list',
+        name: 'task',
+        message: 'What would you like to do?',
+        choices: [
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'View departments',
+            'View roles',
+            'View employees',
+            "Update an employee's role"
+        ]
+    }
+]
+
---
+++ seed.sql
@@ -0,0 +1,31 @@
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''
+
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+USE employee_tracker_db;
+
+CREATE TABLE departments(
+id INT AUTO_INCREMENT,
+name VARCHAR(30),
+PRIMARY KEY (id)
+);
+
+CREATE TABLE role(
+id INT AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL,
+department_id INT,
+PRIMARY KEY(id)
+);
+
+CREATE TABLE employee(
+id INT AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT,
+manager_id INT,
+PRIMARY KEY(id)
+);
+
+
+SELECT * FROM departments;
