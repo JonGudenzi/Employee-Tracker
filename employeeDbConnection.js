b72c0f073197e9453ee8653700aa87d0c7306857
JonGudenzi <jongudenzi@gmail.com>
2021-01-18 19:36:54 -0500

    created connection to employee_tracker_db database

---
+++ employeeDbConnection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Gudenzi1955!',
+    database: 'employee_tracker_db',
+
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    connection.end();
+});
